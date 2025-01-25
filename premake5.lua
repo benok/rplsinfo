f2904e304dd02e532be6dcfd6c7ab833468144b8
benok <benok1@gmail.com>
2025-01-25 19:44:56 +0900

    Add premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,17 @@
+workspace "rplsinfo"
+   configurations { "Debug", "Release" }
+
+project "rplsinfo"
+   kind "ConsoleApp"
+   language "C++"
+   targetdir "bin/%{cfg.buildcfg}"
+
+   files { "**.h", "**.cpp" }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
